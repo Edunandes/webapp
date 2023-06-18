c748464bbd97aa9acabf594d40ba09a11e3ceab1
Eduardo Fernandes <95150616+Edunandes@users.noreply.github.com>
2023-06-17 20:00:59 -0600

    Database update

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,15 @@
+
+
+
+
 module.exports = 
 {
-    //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb://localhost:27017/business_list",
+    //"URI": "mongodb://localhost:27017/business_list"
+    "URI": "mongodb+srv://2723eduardo:31EtqnEtQqMxJJQF@businesslist.fzntaam.mongodb.net/businesslist?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
+
+
+
+// "mongodb+srv://2723eduardo:G$hJ.FJ6tiyD!!R@mongodbserver.xsmtx8k.mongodb.net/business_list?retryWrites=true&w=majority"
+   //TZC5BzNX7iShRIRuEsiFUG2sYKR28ngq0nMNGQm0Z0tKhdIsjfLcgMB7iqlUlwhC
